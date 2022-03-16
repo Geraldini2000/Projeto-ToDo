fa037bf1b12fd998ba7ffe95af51861b7bd8fdd4
Geraldini2000 <geraldinimatos@gmail.com>
2022-03-16 03:40:15 -0300

    Inserindo dados do banco no template

--- tasks/urls.py
+++ tasks/urls.py
@@ -8,5 +8,6 @@ from . import views
 urlpatterns = [
     path('hello', views.hello),
     path('', views.tasksList, name='task-list'),
+    path('task/<int:id>', views.taskView, name='task-view'),
     path('yourname/<str:name>', views.yourName, name='your-name')
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,13 +1,21 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
+from .models import Task
+
+
+def tasksList(request):
+    tasks = Task.objects.all()
+    return render(request, 'tasks/list.html', {'tasks': tasks})
+
+
+def taskView(request, id):
+    task = get_object_or_404(Task, pk=id)
+    return render(request, 'tasks/task.html', {'task': task})
 
 
 def hello(request):
     return HttpResponse('Ola mundo')
-
-
-def tasksList(request):
-    return render(request, 'tasks/list.html')
 
 
 def yourName(request, name):
