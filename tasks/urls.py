9a85163d740ccaec51bbfb272fda7a83088bf5e2
Geraldini2000 <geraldinimatos@gmail.com>
2022-03-16 04:44:01 -0300

    Editando dados no banco de dados

--- tasks/urls.py
+++ tasks/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('', views.tasksList, name='task-list'),
     path('task/<int:id>', views.taskView, name='task-view'),
     path('newtask/', views.newTask, name="new-task"),
+    path('edit/<int:id>', views.editTask, name='edit-task'),
     path('yourname/<str:name>', views.yourName, name='your-name')
 ]
