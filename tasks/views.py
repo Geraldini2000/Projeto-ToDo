011dc4851df5d6a9956a7ed49321ee357361fff3
Geraldini2000 <geraldinimatos@gmail.com>
2022-03-16 18:18:53 -0300

    Adicionando botao de concluir tarefa

--- tasks/views.py
+++ tasks/views.py
@@ -82,6 +82,21 @@ def deleteTask(request, id):
     return redirect('/')
 
 
+@login_required
+def changeStatus(request, id):
+    task = get_object_or_404(Task, pk=id)
+
+    if(task.done == 'doing'):
+        task.done = 'done'
+
+    else:
+        task.done = 'doing'
+
+    task.save()
+
+    return redirect('/')
+
+
 def hello(request):
     return HttpResponse('Ola mundo')
 
