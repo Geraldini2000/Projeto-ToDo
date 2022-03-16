cf239f3b340e7777e34dfa60ce88670021e829fd
Geraldini2000 <geraldinimatos@gmail.com>
2022-03-16 04:17:17 -0300

    Inserindo dados no banco de dados

--- tasks/urls.py
+++ tasks/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('hello', views.hello),
     path('', views.tasksList, name='task-list'),
     path('task/<int:id>', views.taskView, name='task-view'),
+    path('newtask/', views.newTask, name="new-task"),
     path('yourname/<str:name>', views.yourName, name='your-name')
 ]
